82f73c49b4f9f105d96af489017f550d79d13e42
Jay <jay.sanjay.karia@gmail.com>
2022-06-12 12:59:49 +0530

    repeatition remaining

--- Routes/routes.js
+++ Routes/routes.js
@@ -102,4 +102,169 @@ router.post('/submit', (req, res) => {
 
 })
 
+var DBRates = [];
+let RatesObj = [];
+let titles3 = []
+
+router.get('/trending', (req, res) => {
+    globalThis.title;
+    context3 = []
+    let count = 6;
+    // Reading Database
+    let DBProjects = [];
+
+    Rates.find({}, (err, data) => {
+        globalThis.DBProjects;
+        let project = data.map((p) => { return p['Project Name'] });
+        DBProjects = project
+        getProjects(project)
+        return project
+    }
+    );
+    function getProjects(project) {
+        globalThis.DBProjects
+        DBProjects = project
+        DBProjects = [... new Set(DBProjects)]
+        DBProjects = DBProjects.filter(e => e)
+        for (let index = 0; index <= DBProjects.length; index++) {
+            if (DBProjects[index] === undefined || DBProjects === null) {
+                DBProjects[index] = ''
+                DBProjects = DBProjects.filter(e => e)
+            }
+            Rates.find({ 'Project Name': DBProjects[index] }, ((err, data) => {
+                let rates = data.map((ra) => { return ra['Rates'] });
+                let projectName = data.map((pr) => { return pr['Project Name'] });
+                DBRates.push(rates)
+
+                if (projectName[index] === undefined || projectName === null) {
+                    projectName[index] = ''
+                    projectName = projectName.filter(e => e)
+                }
+
+                RatesObj.push(rates + ' ' + projectName)
+                sum = 0
+                avgs = avgs.reverse(avgs.sort(function (a, b) { return a - b }))
+                RatesObj.sort(function (a, b) {
+                    return a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' });
+                });
+
+                RatesObj.reverse();
+
+                RatesObj = [... new Set(RatesObj)]
+                RatesObj = RatesObj.filter(e => e)
+                for (let index = 0; index <= RatesObj.length; index++) {
+                    if (RatesObj[index] === undefined || RatesObj === null) {
+                        RatesObj[index] = ''
+                        RatesObj = RatesObj.filter(e => e)
+                    }
+                }
+                if (index == count - 1) {
+                    RatesObj.forEach((e, index) => {
+                        let [first, ...rest] = RatesObj[index].split(' ')
+                        rest = rest.join(' ')
+                        titles3.push(rest)
+                        if (titles3[index] === '') {
+                            titles3.splice(index, 1)
+                            count = count - 1
+                        }
+                    })
+                    titles3.forEach((e, i) => {
+                        titles3[i] = titles3[i].split(',')[0]
+                    })
+                    // if (titles3.length <= count)
+                    dataDB(DBRates, titles3)
+                }
+            }))
+        }
+    }
+    let list1 = []
+    let cont3 = []
+    let rgbs = []
+
+    let titles2 = []
+    let avgs = []
+    // Data from database
+    function dataDB(r, titles3) {
+        // Other logic
+        // r = [[0], [1], [2], [3], [4], [5]]
+        let counter;
+        let one = '0, 102, 77';
+        let two = '0, 26, 102';
+        let three = '51, 0, 102';
+        let four = '102, 0, 77';
+        let five = '102, 0, 26';
+        let rgb = [one, two, three, four, five];
+
+        let obj = []
+        r.forEach((e, index) => {
+            let R = r[index]
+            let sum = 0;
+            counter = 0;
+            R.forEach((e, index) => {
+                counter = counter + 1
+                sum = sum + e;
+            })
+            avgs.push(sum / counter)
+            if (isNaN(avgs[index]) === true) {
+                avgs[index] = 0;
+            }
+            obj.push(avgs[index] + ' ' + titles[index])
+            sum = 0
+            avgs = avgs.reverse(avgs.sort(function (a, b) { return a - b }))
+            obj.sort(function (a, b) {
+                return a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' });
+            });
+
+            obj.reverse();
+
+        })
+        avgs.forEach((e, index) => {
+            let avg = avgs[index]
+            if (avg >= 0 && avg <= 1) {
+                rgbs.push(rgb[0])
+            } else if (avg > 1 && avg <= 2) {
+                rgbs.push(rgb[1])
+            } else if (avg > 2 && avg <= 3) {
+                rgbs.push(rgb[2])
+            } else if (avg > 3 && avg <= 4) {
+                rgbs.push(rgb[3])
+            } else if (avg > 4 && avg <= 5) {
+                rgbs.push(rgb[4])
+            }
+            avgs[index] = Math.round(avgs[index] * 10) / 10
+        })
+        obj.forEach((e, index) => {
+            let [first, ...rest] = obj[index].split(' ')
+            rest = rest.join(' ')
+            titles2.push(rest)
+        })
+
+
+        // titles = titles.reverse();
+        titles3.forEach((e, index) => {
+            list1.push(r[titles3[index]])
+            try {
+                cont3 = {
+                    'titles': titles3[index],
+                    'avgs': avgs[index],
+                    'rgb': rgbs[index]
+                }
+                context3.push(cont3)
+            } catch (e) { }
+        })
+
+
+        if (context3.length > r.length) {
+            context3 = context3.splice(0, r.length)
+        }
+        list1 = list1.reverse(list1.sort())
+        res.render('trending', {
+            'avgs': avgs,
+            'titles3': titles3,
+            'rgb': rgbs,
+            "context": context3
+        })
+    }
+})
+
 module.exports = router;
